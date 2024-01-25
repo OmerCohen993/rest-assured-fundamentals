7b306881f177402e88ba7f667cd0bedda4a8bb63
OmerCohen993 <omerplace@gmail.com>
2024-01-25 23:02:06 +0200

    lesson11

--- src/test/java/config/VideoGameConfig.java
+++ src/test/java/config/VideoGameConfig.java
@@ -6,13 +6,15 @@ import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import org.junit.BeforeClass;
 
-public class TestConfig {
+public class VideoGameConfig {
     @BeforeClass
     public static void setup(){
-        RestAssured.baseURI = "https://videogamedb.uk/";
-        RestAssured.basePath = "api/v2/";
+//        RestAssured.baseURI = "https://videogamedb.uk/";
+//        RestAssured.basePath = "api/v2/";
 //        RestAssured.port = 443;
         RestAssured.requestSpecification = new RequestSpecBuilder()
+                .setBaseUri("https://videogamedb.uk/")
+                .setBasePath("api/v2/")
                 .setContentType("application/json")
                 .addHeader("Accept", "application/json")
                 .build();
