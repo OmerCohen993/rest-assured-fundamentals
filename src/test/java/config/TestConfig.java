6d4ead4e61938d754c9fe5c2cb8a992ef1be5ba1
OmerCohen993 <omerplace@gmail.com>
2024-01-25 22:46:53 +0200

    Lesson8

---
+++ src/test/java/config/TestConfig.java
@@ -0,0 +1,21 @@
+package config;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.BeforeClass;
+
+public class TestConfig {
+    @BeforeClass
+    public static void setup(){
+        RestAssured.baseURI = "https://videogamedb.uk/";
+        RestAssured.basePath = "api/v2/";
+//        RestAssured.port = 443;
+
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+                .setContentType("application/json")
+                .addHeader("Accept", "application/json")
+                .build();
+
+    }
+}
