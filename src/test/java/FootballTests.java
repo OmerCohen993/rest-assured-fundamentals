552be37be3d902728b3736b81b6353db31e1534d
OmerCohen993 <omerplace@gmail.com>
2024-01-27 16:47:34 +0200

    lesson21

--- src/test/java/FootballTests.java
+++ src/test/java/FootballTests.java
@@ -2,6 +2,7 @@ import config.FootballCongig;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class FootballTests extends FootballCongig {
     @Test
@@ -14,17 +15,34 @@ public class FootballTests extends FootballCongig {
 
     }
 
+
     @Test
     public void getDetailsOfMultipleAreas() {
         String areasId = "200,2001";
         given()
                 .urlEncodingEnabled(false) //doesn't need
-                .queryParam("areas" , areasId)
+                .queryParam("areas", areasId)
                 .when()
                 .get("/areas")
                 .then();
 
     }
 
+    @Test
+    public void getDataFounded() {
+        given()
+                .when()
+                .get("teams/57")
+                .then()
+                .body("founded", equalTo(1886));
+    }
 
+    @Test
+    public void getFirstTeamName() {
+        given()
+                .when()
+                .get("competitions/2021/teams")
+                .then()
+                .body("teams.name[0]", equalTo("Arsenal FC"));
+    }
 }
