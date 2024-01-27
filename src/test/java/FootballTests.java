dd6d774b7e87715744eda082f6c1bd60a04f1a13
OmerCohen993 <omerplace@gmail.com>
2024-01-27 16:27:02 +0200

    lesson20

--- src/test/java/FootballTests.java
+++ src/test/java/FootballTests.java
@@ -1,5 +1,30 @@
 import config.FootballCongig;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class FootballTests extends FootballCongig {
+    @Test
+    public void getDetailsOfOneArea() {
+        given()
+                .queryParam("areas", 2007)
+                .when()
+                .get("/areas")
+                .then();
+
+    }
+
+    @Test
+    public void getDetailsOfMultipleAreas() {
+        String areasId = "200,2001";
+        given()
+                .urlEncodingEnabled(false) //doesn't need
+                .queryParam("areas" , areasId)
+                .when()
+                .get("/areas")
+                .then();
+
+    }
+
 
 }
